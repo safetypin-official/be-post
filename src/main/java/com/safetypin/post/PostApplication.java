e5d1536ed0b4e9a5f370aac52c012ccbc800e4c7
Muhammad Raihan Akbar <ianakbar711@gmail.com>
2025-02-14 19:39:28 +0700

    add: Hello World

--- src/main/java/com/safetypin/post/PostApplication.java
+++ src/main/java/com/safetypin/post/PostApplication.java
@@ -2,6 +2,9 @@ package com.safetypin.post;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class PostApplication {
@@ -10,4 +13,13 @@ public class PostApplication {
 		SpringApplication.run(PostApplication.class, args);
 	}
 
+}
+
+@RestController
+@RequestMapping("/")
+class HelloController {
+	@GetMapping
+	public String sayHello() {
+		return "Hello, World!";
+	}
 }
