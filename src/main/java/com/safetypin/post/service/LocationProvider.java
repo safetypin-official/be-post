ce1bcaf5e91a6f9c9005755703604371382b257b
KronosDP <darrel.danadyaksa19@gmail.com>
2025-03-01 20:53:11 +0700

    [GREEN] Add LocationProvider interface for location data services

---
+++ src/main/java/com/safetypin/post/service/LocationProvider.java
@@ -0,0 +1,29 @@
+package com.safetypin.post.service;
+
+import com.safetypin.post.model.Location;
+
+/**
+ * Interface for services that provide location data
+ */
+public interface LocationProvider {
+    
+    /**
+     * Check if location services are enabled
+     */
+    boolean isLocationEnabled();
+    
+    /**
+     * Get the current device location
+     */
+    Location getCurrentLocation();
+    
+    /**
+     * Get the last manually set location
+     */
+    Location getLastManualLocation();
+    
+    /**
+     * Save a manually set location
+     */
+    void saveManualLocation(Location location);
+}
