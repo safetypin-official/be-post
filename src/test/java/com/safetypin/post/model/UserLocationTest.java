d23bd27ec75216d1822e8ab4eb004b3e7cbd688d
KronosDP <darrel.danadyaksa19@gmail.com>
2025-03-01 20:48:13 +0700

    [RED] Add unit tests for UserLocation to validate constructors and setter methods

---
+++ src/test/java/com/safetypin/post/model/UserLocationTest.java
@@ -0,0 +1,42 @@
+package com.safetypin.post.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserLocationTest {
+    
+    @Test
+    public void testDefaultConstructor() {
+        UserLocation location = new UserLocation();
+        assertEquals(0.0, location.getLatitude(), 0.0001);
+        assertEquals(0.0, location.getLongitude(), 0.0001);
+    }
+    
+    @Test
+    public void testParameterizedConstructor() {
+        double latitude = 37.7749;
+        double longitude = -122.4194;
+        UserLocation location = new UserLocation(latitude, longitude);
+        
+        assertEquals(latitude, location.getLatitude(), 0.0001);
+        assertEquals(longitude, location.getLongitude(), 0.0001);
+    }
+    
+    @Test
+    public void testSetLatitude() {
+        UserLocation location = new UserLocation();
+        double latitude = 40.7128;
+        
+        location.setLatitude(latitude);
+        assertEquals(latitude, location.getLatitude(), 0.0001);
+    }
+    
+    @Test
+    public void testSetLongitude() {
+        UserLocation location = new UserLocation();
+        double longitude = -74.0060;
+        
+        location.setLongitude(longitude);
+        assertEquals(longitude, location.getLongitude(), 0.0001);
+    }
+}
