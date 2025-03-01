5f539d95cec122e496fb2934c752d326c08084c7
riorio805 <sefrianojieftara@gmail.com>
2025-03-01 23:20:38 +0700

    Create placeholder service implementation to enable autowiring services in other service implementations Prevents java.lang.IllegalStateException: Failed to load ApplicationContext when running application.

---
+++ src/main/java/com/safetypin/post/service/UserLocationServiceImpl.java
@@ -0,0 +1,20 @@
+package com.safetypin.post.service;
+
+import com.safetypin.post.model.UserLocation;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserLocationServiceImpl implements UserLocationService {
+    @Override
+    public UserLocation getCurrentUserLocation() {
+        // TODO Implement getCurrentUserLocation
+        return null;
+    }
+
+    @Override
+    public UserLocation updateUserLocation(double latitude, double longitude) {
+        // TODO Implement updateUserLocation
+        return null;
+    }
+}
