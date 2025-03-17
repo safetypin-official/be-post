573c4fc173033817d3bea5aadacdb9e6b3d3e501
Muhammad Raihan Akbar <ianakbar711@gmail.com>
2025-03-17 19:01:17 +0700

    [GREEN] Implement S3 Controller

---
+++ src/main/java/com/safetypin/post/controller/S3Controller.java
@@ -0,0 +1,25 @@
+package com.safetypin.post.controller;
+
+import com.safetypin.post.service.S3Service;
+import org.springframework.web.bind.annotation.*;
+import java.net.URL;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/post/s3")
+public class S3Controller {
+
+    private final S3Service s3Service;
+
+    public S3Controller(S3Service s3Service) {
+        this.s3Service = s3Service;
+    }
+
+    // Endpoint untuk generate pre-signed URL
+    @PostMapping("/presigned-url")
+    public Map<String, String> getPresignedUrl(@RequestBody Map<String, String> request) {
+        String fileType = request.get("fileType"); // e.g., "jpeg", "png"
+        URL presignedUrl = s3Service.generatePresignedUrl(fileType);
+        return Map.of("url", presignedUrl.toString());
+    }
+}
