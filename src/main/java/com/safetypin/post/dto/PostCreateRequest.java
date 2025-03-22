f8646a1b4e636d581a5cd356351dbb3ecc7702a1
KronosDP <darrel.danadyaksa19@gmail.com>
2025-03-22 12:54:18 +0700

    [REFACTOR] enhance PostCreateRequest by adding JSON property annotations for required fields

--- src/main/java/com/safetypin/post/dto/PostCreateRequest.java
+++ src/main/java/com/safetypin/post/dto/PostCreateRequest.java
@@ -1,12 +1,22 @@
 package com.safetypin.post.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class PostCreateRequest {
+    @JsonProperty(value = "Title", required = true)
     private String title;
+    
+    @JsonProperty(value = "Caption", required = true)
     private String caption;
+    
+    @JsonProperty(value = "Latitude", required = true)
     private Double latitude;
+    
+    @JsonProperty(value = "Longitude", required = true)
     private Double longitude;
+    
+    @JsonProperty(value = "Category", required = true)
     private String category;
 }
