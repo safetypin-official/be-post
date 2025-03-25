4dc90c3275d05f51726b803e885c9759eb49c285
Fredo <fredotanzil@gmail.com>
2025-03-26 03:01:20 +0700

    [GREEN] Implement Vote model

---
+++ src/main/java/com/safetypin/post/model/Vote.java
@@ -0,0 +1,35 @@
+package com.safetypin.post.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "votes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Vote {
+
+    @Embeddable
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class VoteId implements Serializable {
+        private UUID userId;
+
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "post_id")
+        private Post post;
+    }
+
+    @EmbeddedId
+    private VoteId id;
+
+    @Column(nullable = false)
+    private boolean isUpvote; // true for upvote, false for downvote
+}
