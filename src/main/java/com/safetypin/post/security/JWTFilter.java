ad359e9999b4c21b06349614cdaabb63cfc03032
riorio805 <sefrianojieftara@gmail.com>
2025-03-28 21:03:26 +0800

    [RED] Create JWTFilter skeleton extending OncePerRequestFilter and using JWTUtil

---
+++ src/main/java/com/safetypin/post/security/JWTFilter.java
@@ -0,0 +1,27 @@
+package com.safetypin.post.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class JWTFilter extends OncePerRequestFilter {
+    private final JWTUtil jwtUtil;
+
+    public JWTFilter(JWTUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
