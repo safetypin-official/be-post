82591c3a69b4703a32df6fcdb24f82f10ef20e54
KronosDP <darrel.danadyaksa19@gmail.com>
2025-04-28 13:37:24 +0700

    [REFACTOR] Update request mapping in CommentNotificationController for consistency

--- src/main/java/com/safetypin/post/controller/CommentNotificationController.java
+++ src/main/java/com/safetypin/post/controller/CommentNotificationController.java
@@ -22,7 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
-@RequestMapping("/post/comment-notifications") // Updated mapping
+@RequestMapping("/posts/comment-notifications") // Updated mapping
 @AllArgsConstructor
 public class CommentNotificationController { // Renamed controller
 
