57fecb68a68889d084d7fa73344b8b03e771b254
KronosDP <darrel.danadyaksa19@gmail.com>
2025-04-28 14:13:49 +0700

    [REFACTOR] Organize imports and update request mapping in CommentController for consistency

--- src/main/java/com/safetypin/post/controller/CommentController.java
+++ src/main/java/com/safetypin/post/controller/CommentController.java
@@ -1,5 +1,26 @@
 package com.safetypin.post.controller;
 
+import java.util.Map;
+import java.util.UUID;
+import java.util.function.Supplier;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.safetypin.post.dto.CommentDTO;
 import com.safetypin.post.dto.CommentRequest;
 import com.safetypin.post.dto.PostResponse;
@@ -10,25 +31,13 @@ import com.safetypin.post.exception.UnauthorizedAccessException;
 import com.safetypin.post.model.CommentOnComment;
 import com.safetypin.post.model.CommentOnPost;
 import com.safetypin.post.service.CommentService;
+
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
-import java.util.UUID;
-import java.util.function.Supplier;
 
 @Slf4j
 @RestController
-@RequestMapping("/post/comment")
+@RequestMapping("/posts/comment")
 @AllArgsConstructor
 public class CommentController {
 
